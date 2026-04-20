f654d68fda321daa70b9652680d29a7336ab364a
joshua.partington <joshua.partington@brighthr.com>
2026-04-20 13:59:57 +0100

    Add Aspire.Hosting.Redis package reference and update project files

--- RateLimitingBackendGuildProject.AppHost/AppHost.cs
+++ RateLimitingBackendGuildProject.AppHost/AppHost.cs
@@ -1,8 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var redis = builder.AddRedis("redis");
+
 var apiService = builder.AddProject<Projects.RateLimitingBackendGuildProject_ApiService>("apiservice")
     .WithHttpHealthCheck("/health")
-    .WithReplicas(2);
+    .WithReplicas(2)
+    .WithReference(redis);
 
 builder.AddProject<Projects.LoadSimulator>("loadsimulator")
     .WithReference(apiService)
